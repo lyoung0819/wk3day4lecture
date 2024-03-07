3d4c980c687592103258c84184cb7617d45a59e0
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-03-07 13:54:40 -0600

    added login and log out functionality

--- app/blog.py
+++ app/blog.py
@@ -4,6 +4,7 @@ class Blog:
     def __init__(self):
         self.users = []
         self.posts = []
+        self.current_user = None # If user is not logged in, then it will equal None. Once logged in, it will equal that instance 
 
 
     # Method to create a new user instance and add to the Blog's user list 
@@ -20,3 +21,31 @@ class Blog:
             # Add the new instance of the user to the blog's user list
             self.users.append(new_user)
             print(f"{new_user} has been created.")
+
+
+    # Method to log a user in
+    def log_user_in(self):
+        # Ask the user for their creds 
+        username_input = input('What is your username? ')
+        password_input = input('What is your password? ')
+        # Loop through the users in the blog user list 
+        for user in self.users:
+            # Check if the user's username matches the user input - same with password
+            if user.username == username_input and user.check_password(password_input):
+                # If both are True, set the blog's current user to that user
+                self.current_user = user
+                print(f"{user} has logged in.")
+                # Once we find the right user, we don't need to to check other users so break
+                break
+        # if we go through the entire loop without breaking # AN ELSE AFTER A FOR WILL RUN IF THE FOR DOES NOT BREAK
+        else: 
+            # Then the user has bad creds
+            print('Username and/or password is incorrect.')
+
+    # Method to log a user out
+    def log_user_out(self):
+        username = self.current_user.username
+        self.current_user = None
+        print(f"{username} has been logged out.")
+
+    
--- blogrun.py
+++ blogrun.py
@@ -7,21 +7,31 @@ def run_blog():
     blog = Blog()
     #start running our blog until user quits
     while True:
-        # Print menu options
-        print(f"1. Sign Up\n2. Login\n3. View All Posts\n4. View Single Posts\n5. Quit")
-        # Ask the user what they want to do
-        to_do = input('Which option would you like to do?')
-        # Make sure they give a valid option 
-        while to_do not in {'1','2','3','4','5'}:
-            to_do = input('Inavlid option. Please enter 1, 2, 3, 4, or 5: ') # Re-define the to_do variable 
-        # Different routes/options
-        if  to_do == '5':
-            break
-        elif to_do == '1':
-            # Call create new user method on the blog
-            blog.create_new_user()
+        if blog.current_user is None:
+            # Print logged out menu options
+            print(f"1. Sign Up\n2. Login\n3. View All Posts\n4. View Single Posts\n5. Quit")
+            # Ask the user what they want to do
+            to_do = input('Which option would you like to do?')
+            # Make sure they give a valid option 
+            while to_do not in {'1','2','3','4','5'}:
+                to_do = input('Inavlid option. Please enter 1, 2, 3, 4, or 5: ') # Re-define the to_do variable 
+            # Different routes/options
+            if  to_do == '5':
+                break
+            elif to_do == '1':
+                # Call create new user method on the blog
+                blog.create_new_user()
+            elif to_do == '2':
+                # Call log in method on the blog
+                blog.log_user_in() 
+            else:
+                print(f'Option {to_do} is coming soon.')
         else:
-            print(f'Option {to_do} is coming soon.')
+            # Print the menu for logged in users
+            print('1. Sign Out')
+            to_do = input('Which option would you like to do? ')
+            if to_do == '1':
+                blog.log_user_out()
     # Once the user quits and while loop breaks
     print('Thank you for checking out the blog!')
     print(blog.users)

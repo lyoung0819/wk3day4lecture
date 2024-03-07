1d0ea72b134527d43046c8b8c88bcdc1f142842e
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-03-07 11:51:44 -0600

    progress save

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+from .blog import Blog
+
+# This is the entry point into this module
+# We need to import so we can export elsewhere
--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User:
         self.id = User.id_counter
         User.id_counter += 1
         self.username = username
-        self.__password = password #this will be a private variable, password attribute can only be accessed within the class. We want to hide this 
+        self.__password = password #this will be a private variable, password attribute can only be accessed within the class.  
 
     def __str__(self):
         return self.username
@@ -17,10 +17,3 @@ class User:
     
     def check_password(self, password_guess):
         return self.__password == password_guess
-    
-u = User('brians', '123abc')
-
-print(u.check_password('123abc'))
-print(u.check_password('abc123'))
-
-print(u.__password)
